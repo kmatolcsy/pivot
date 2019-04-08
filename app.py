0139b3adc0f080fe56c4e080132d3e6b1c34bc6d
Kristóf <matolcsy.kristof@gmail.com>
2019-04-08 13:45:19 +0200

    Dropdowns for x and y

--- app.py
+++ app.py
@@ -4,18 +4,19 @@ import webbrowser as wb
 import datetime as dt
 import pandas as pd
 import dash
+import dash_table
 import dash_html_components as html
 import dash_core_components as dcc
 
 from flask import Flask, render_template
+from dash.dependencies import Input, Output
 
 
 class Directory:
     def __init__(self, path):
         self.path = path
         self.path_list = path.split('/')
-
-        self.data_ext = ['.csv', '.xlsx']
+        self.data_ext = ['.csv', 'xls', '.xlsx']
 
     def is_data(self, file_name):
         _, ext = os.path.splitext(file_name)
@@ -28,26 +29,74 @@ class Directory:
         return [entry for entry in os.scandir(self.path) if entry.is_file() and self.is_data(entry.name)]
 
 
+# flask server / file explorer
 server = Flask(__name__)
+# dash application
 app = dash.Dash(__name__, server=server, assets_folder='static', url_base_pathname='/dash_base/')
 app.layout = html.Div('initial layout')
+app.config['suppress_callback_exceptions'] = True
+# working dir
 os.chdir(os.path.expanduser('~'))
 
 
+def read_data(path, *kwargs):
+    _, ext = os.path.splitext(path)
+
+    if ext == '.csv':
+        df = pd.read_csv(path, *kwargs)
+    else:
+        df = pd.read_excel(path, *kwargs)
+
+    return df
+
+
 def layout(path):
-    df = pd.read_csv(path, index_col=0)
+    df = read_data(path)
+    dd_options = [{'label': col, 'value': col} for col in df.columns.values]
+
     return html.Div(
+        id='layout',
         className='container',
         children=[
-            dcc.Graph(
-                figure={
-                    'data': [
-                        {'x': df.index, 'y': df.iloc[:, 0], 'type': 'line', 'name': 'name'}],
-                    'layout': {
-                        'title': 'Dash Data Visualization'}
-                }
+            html.Button(
+                id='path',
+                className='btn btn-secondary',
+                children='btn',
+                type='button',
+                title=path,
+                **{'data-toggle': 'tooltip', 'data-placement': 'left'}
+            ),
+            dcc.Dropdown(
+                id='x-axis',
+                options=dd_options,
+                value=dd_options[0]['value']
+            ),
+            dcc.Dropdown(
+                id='y-axis',
+                options=dd_options,
+                multi=True,
+                value=dd_options[-1]['value']
+            ),
+            html.Div(
+                id='graph'
             )
         ]
+    )
+
+
+@app.callback(
+    Output('graph', 'children'),
+    [Input('x-axis', 'value'), Input('y-axis', 'value'), Input('path', 'title')])
+def graph(idx, columns, path):
+    print(idx)
+    df = read_data(path)
+    columns = [columns] if isinstance(columns, str) else columns
+
+    return dcc.Graph(
+        figure={
+            'data': [{'x': df[idx], 'y': df[column], 'type': 'line', 'name': column} for column in columns],
+            'layout': {'title': 'Dash Data Visualization'}
+        }
     )
 
 
@@ -74,10 +123,10 @@ def plot(path):
     return app.index()
 
 
-t = threading.Thread(target=server.run)
-t.start()
-wb.open_new('http://localhost:5000/')
+# t = threading.Thread(target=server.run)
+# t.start()
+# wb.open_new('http://localhost:5000/')
 
 if __name__ == '__main__':
-    pass
+    server.run(debug=True)
 
